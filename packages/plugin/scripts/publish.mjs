2b4f001eaa7fc2a68197b9626b7cb761c7dd8492
macintoshhelper <6757532+macintoshhelper@users.noreply.github.com>
2024-10-07 14:27:32 +0400

    0.0.5 changeset

--- packages/plugin/scripts/publish.mjs
+++ packages/plugin/scripts/publish.mjs
@@ -1,31 +1,48 @@
+import { readFile, writeFile } from 'fs/promises';
+import path from 'path';
+
 import { extractChangeset } from "./parse-changeset.mjs";
+
+// import * as fs from 'fs-extra';
+import * as url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const __filename = url.fileURLToPath(import.meta.url);
+
+async function readJSONFile(filePath) {
+  const absoluteFilePath = path.resolve(filePath)
+  const data = await readFile(absoluteFilePath, 'utf8');
+  return JSON.parse(data);
+}
 
 async function main() {
   console.log('main()');
-  const args = process.argv.slice(2); // Skip the first two arguments (node and script path)
-  // const published = args[0]; // The first argument passed from GitHub Actions
+  // const args = process.argv.slice(2); // Skip the first two arguments (node and script path)
+  // // const published = args[0]; // The first argument passed from GitHub Actions
 
-  const dataIndex = args.indexOf('--data');
+  // const dataIndex = args.indexOf('--data');
   
-  const data = dataIndex !== -1 && args[dataIndex + 1] ? args[dataIndex + 1] : 'default';
+  // const data = dataIndex !== -1 && args[dataIndex + 1] ? args[dataIndex + 1] : 'default';
 
-  console.log({ data });
+  // console.log({ data });
 
-  let pluginVersion;
-  try {
-    const publishedVersions = JSON.parse(data);
+  // let pluginVersion;
+  // try {
+  //   const publishedVersions = JSON.parse(data);
 
-    pluginVersion = publishedVersions.find(({ name }) => (name === '@elemental-figma/json-to-figma'))?.version;
+  //   pluginVersion = publishedVersions.find(({ name }) => (name === '@elemental-figma/json-to-figma'))?.version;
 
-  } catch (err) {
-    console.log(err);
-    return;
-  }
-  if (!pluginVersion) {
-    console.log('Plugin version change not found.');
-    return;
-  }
-  const changes = extractChangeset('@elemental-figma/json-to-figma', pluginVersion);
+  // } catch (err) {
+  //   console.log(err);
+  //   return;
+  // }
+  // if (!pluginVersion) {
+  //   console.log('Plugin version change not found.');
+  //   return;
+  // }
+  const packageJson = await readJSONFile(path.join(__dirname, '../package.json'));
+
+  const changes = extractChangeset('@elemental-figma/json-to-figma', packageJson.version);
 
   // [{"name": "@xx/xx", "version": "1.2.0"}, {"name": "@xx/xy", "version": "0.8.9"}]
   
