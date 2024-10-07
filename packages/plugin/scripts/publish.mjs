496a9846eea5805836a116f987a6c1e7f06a75cc
macintoshhelper <6757532+macintoshhelper@users.noreply.github.com>
2024-10-07 13:28:21 +0400

    test

--- packages/plugin/scripts/publish.mjs
+++ packages/plugin/scripts/publish.mjs
@@ -1,7 +1,15 @@
 async function main() {
+  console.log('main()');
   const args = process.argv.slice(2); // Skip the first two arguments (node and script path)
-  const published = args[0]; // The first argument passed from GitHub Actions
-  console.log(`Published status: ${published}`);
+  // const published = args[0]; // The first argument passed from GitHub Actions
+
+  const dataIndex = args.indexOf('--data');
+  
+  const data = dataIndex !== -1 && args[dataIndex + 1] ? args[dataIndex + 1] : 'default';
+  
+  
+
+  console.log(`Published status: ${data}`);
 
   console.log('Publish');
 }
