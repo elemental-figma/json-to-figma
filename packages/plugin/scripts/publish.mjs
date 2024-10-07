e96a6fee537eed713edd99b73847e59b59fcb098
macintoshhelper <6757532+macintoshhelper@users.noreply.github.com>
2024-10-07 13:54:45 +0400

    add changelog parsing script

--- packages/plugin/scripts/publish.mjs
+++ packages/plugin/scripts/publish.mjs
@@ -1,3 +1,5 @@
+import { extractChangeset } from "./parse-changeset.mjs";
+
 async function main() {
   console.log('main()');
   const args = process.argv.slice(2); // Skip the first two arguments (node and script path)
@@ -6,10 +8,30 @@ async function main() {
   const dataIndex = args.indexOf('--data');
   
   const data = dataIndex !== -1 && args[dataIndex + 1] ? args[dataIndex + 1] : 'default';
+
+  console.log({ data });
+
+  let pluginVersion;
+  try {
+    const publishedVersions = JSON.parse(data);
+
+    pluginVersion = publishedVersions.find(({ name }) => (name === '@elemental-figma/json-to-figma'))?.version;
+
+  } catch (err) {
+    console.log(err);
+    return;
+  }
+  if (!pluginVersion) {
+    console.log('Plugin version change not found.');
+    return;
+  }
+  const changes = extractChangeset('@elemental-figma/json-to-figma', pluginVersion);
+
+  // [{"name": "@xx/xx", "version": "1.2.0"}, {"name": "@xx/xy", "version": "0.8.9"}]
   
   
 
-  console.log(`Published status: ${data}`);
+  console.log(`Published status: ${changes}`);
 
   console.log('Publish');
 }
